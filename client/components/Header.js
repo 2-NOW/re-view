4f230402bc007644f8447944c3f79372ec2a113a
2-NOW <kj109888@gmail.com>
2021-09-08 19:44:57 +0900

    wip

--- client/components/Header.js
+++ client/components/Header.js
@@ -5,32 +5,69 @@ import { useRouter } from 'next/router';
 export default function Header({ placeholder }) {
   const router = useRouter();
   return (
-    <>
+    <HeaderSection>
       <div className="headInner">
         <div className="logo" onClick={() => router.push('/')}>
           <span>KDIER</span>
         </div>
+        <nav>
+          <a href="#">강의검색</a>
+          <a href="#">시간표</a>
+        </nav>
+        {/* 모바일 메뉴 */}
+        <form className="search">
+          <input
+            type="text"
+            placeholder={placeholder ? placeholder : 'class name or number'}
+            required
+          />
+          <button type="submit">
+            <Search />
+          </button>
+        </form>
+        <div className="profile">
+          <a href="#" className="user">
+            <User className="userIcon" />
+          </a>
+        </div>
       </div>
-      <nav>
-        <a href="#">강의검색</a>
-        <a href="#">시간표</a>
-      </nav>
-      {/* 모바일 메뉴 */}
-      <form className="search">
-        <input
-          type="text"
-          placeholder={placeholder ? placeholder : 'class name or number'}
-          required
-        />
-        <button type="submit">
-          <Search />
-        </button>
-      </form>
-      <div className="profile">
-        <a href="#" className="user">
-          <User className="userIcon" />
-        </a>
-      </div>
-    </>
+    </HeaderSection>
   );
 }
+
+const HeaderSection = styled.header`
+  position: fixed;
+  top: 0;
+  color: #fafafc;
+  background-color: red;
+  width: 100%;
+
+  input {
+    width: 100%;
+  }
+
+  .user,
+  .profile,
+  .logo,
+  nav {
+    display: flex;
+  }
+
+  .headerInner {
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+  }
+
+  nav {
+    flex: 1;
+    justify-content: center;
+    a {
+      position: relative;
+    }
+  }
+
+  .profile {
+    justify-content: flex-end;
+  }
+`;
