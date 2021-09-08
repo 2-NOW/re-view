2e33c5aae265219c8c164594680b940dbdaf2e74
2-NOW <kj109888@gmail.com>
2021-09-08 23:42:46 +0900

    wip

--- client/components/Header.js
+++ client/components/Header.js
@@ -38,36 +38,67 @@ export default function Header({ placeholder }) {
 const HeaderSection = styled.header`
   position: fixed;
   top: 0;
-  color: #fafafc;
-  background-color: red;
-  width: 100%;
-
-  input {
-    width: 100%;
-  }
+  color: red;
+  // width: 100%;
+  border: 1px solid;
 
   .user,
   .profile,
   .logo,
   nav {
     display: flex;
+    align-items: center;
   }
 
   .headerInner {
-    margin: 0 auto;
-    display: grid;
+    display: flex;
     grid-template-columns: 1fr 2fr 1fr;
   }
 
   nav {
+    border: 1px solid;
     flex: 1;
     justify-content: center;
-    a {
-      position: relative;
-    }
   }
 
-  .profile {
-    justify-content: flex-end;
+  form {
+    border: 1px solid;
   }
 `;
+
+// const HeaderSection = styled.header`
+//   position: fixed;
+//   top: 0;
+//   color: #fafafc;
+//   background-color: red;
+//   width: 100%;
+
+//   input {
+//     width: 100%;
+//   }
+
+//   .user,
+//   .profile,
+//   .logo,
+//   nav {
+//     display: flex;
+//   }
+
+//   .headerInner {
+//     margin: 0 auto;
+//     display: grid;
+//     grid-template-columns: 1fr 2fr 1fr;
+//   }
+
+//   nav {
+//     flex: 1;
+//     justify-content: center;
+//     a {
+//       position: relative;
+//     }
+//   }
+
+//   .profile {
+//     justify-content: flex-end;
+//   }
+// `;
