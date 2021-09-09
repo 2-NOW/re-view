151f4d9240f4b70dd5786cb7d401a8166710af7b
2-NOW <kj109888@gmail.com>
2021-09-09 19:57:21 +0900

    Header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,14 @@
-import { Button, makeStyles, Toolbar } from '@material-ui/core';
+import { Button, IconButton, makeStyles, Toolbar } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonIcon from '@material-ui/icons/Person';
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
+    justifyContent: `space-between`,
   },
+  logo: {},
+  searchIcon: {},
 }));
 
 export default function Header() {
@@ -12,7 +17,18 @@ export default function Header() {
   return (
     <>
       <Toolbar className={classes.toolbar}>
-        <Button size="large">KIDER</Button>
+        <Button className={classes.logo} size="large">
+          KIDER
+        </Button>
+        <div className="menu-items">
+          <Button size="large">Search</Button>
+          <Button size="large">TimeLine</Button>
+          <Button size="large">Community</Button>
+          <Button size="large">Market</Button>
+        </div>
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
       </Toolbar>
     </>
   );
