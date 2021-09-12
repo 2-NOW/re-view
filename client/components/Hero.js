726b32135dd74895e31bd9e7374051e13cf4975e
2-NOW <kj109888@gmail.com>
2021-09-12 22:48:45 +0900

    wip

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -4,7 +4,7 @@ import { Container, makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
   heroContent: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(10, 10, 6),
+    padding: theme.spacing(10, 50, 6),
     border: `1px solid`,
   },
 }));
@@ -14,7 +14,7 @@ export default function Hero() {
 
   return (
     <>
-      <Container className={classes.heroContent} maxWidth="lg">
+      <Container className={classes.heroContent}>
         <div>Hero</div>
         <Search />
       </Container>
